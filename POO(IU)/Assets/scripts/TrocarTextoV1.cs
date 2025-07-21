ae4febcbb6b1801f68474b27a5c12308d6cf196e
cauanmat16 <cauan.mateus.mateus@gmail.com>
2025-07-21 10:22:14 -0300

    21/07

---
+++ POO(IU)/Assets/scripts/TrocarTextoV1.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+
+public class TrocarTextoV1 : MonoBehaviour
+{
+     public string texto;
+     public TextMeshProUGUI textoUI;
+
+
+     public void TrocarTextoUI()
+     {
+         textoUI.text = texto;
+     }
+}
